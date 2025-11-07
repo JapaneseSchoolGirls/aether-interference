639698be9463073aaedbcfe7a77254336df622b6
JapaneseSchoolGirls <khatan152@gmail.com>
2025-11-06 23:22:42 -0500

    aether interference.lua

---
+++ aether interference.lua
@@ -0,0 +1,13 @@
+local key = ""
+
+--[[
+
+keybind E: For fake right side dash (you can do the m1 reset with this)
+keybind T: For a front side dash (useful for after shove)
+Shove : act as a backdash cancel basically teleporting you to the player in range of a distance
+
+
+]]
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JapaneseSchoolGirls/aether-interference/refs/heads/main/tsb%20lock%20on%20script.lua"))(key)
